779418b9aa0652815c6052c348195fce89e6cda1
Joshua Wehr <wehrjosh@hs-furtwangen.de>
2020-06-04 13:09:31 +0200

    Zum zweiten mal hochladen F

--- src/main/java/de/hfu/Schritt3.java
+++ src/main/java/de/hfu/Schritt3.java
@@ -1,5 +1,11 @@
+package de.hfu;
 import java.util.*;
 public class Schritt3{
+	/**
+	 *Dieses Programm ändert Klein in Großbuchstaben um. 
+	 *Leider funktioniert es nur mit einem String, daher is die if Bediengung unnötig.
+	 *Ein dritter Kommentar!
+	 */
 	public static void main(String[] args) {
 		System.out.println("Bitte Wort zum umwandeln eingeben: \n");
 		Scanner scanner=new Scanner(System.in);
